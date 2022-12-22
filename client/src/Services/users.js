1eeb8d42f25a5f85c6b7833fb34cd2b0c1a12f00
KennyWorth <93877201+KennyWorth@users.noreply.github.com>
2022-12-22 03:28:49 -0600

    clean up api calls

--- client/src/Services/users.js
+++ client/src/Services/users.js
@@ -8,28 +8,3 @@ export const login = async (username, password) => {
     return response.data;
 }
 
-export const getAllUsers = async () => {
-    const response = await api.get("/users");
-    return response.data;
-}
-
-export const getUserById = async (id) => {
-    const response = await api.get(`/users/${id}`);
-    return response.data;
-}
-
-export const createUser = async (user) => {
-    const response = await api.post("/users", user);
-    return response.data;
-}
-
-export const updateUser = async (id, user) => {
-    const response = await api.put(`/users/${id}`, user);
-    return response.data;
-}
-
-export const deleteUser = async (id) => {
-    const response = await api.delete(`/users/${id}`);
-    return response.data;
-}
-
