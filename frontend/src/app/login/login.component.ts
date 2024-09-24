289d54a1bc18f0fbc8863d6d7187607b3dce6bc0
Philip T <philip1fox1@gmail.com>
2024-09-23 17:38:13 -0700

    login page and routing finished

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    //TODO check if login is valid and input not empty
+    //TODO also to navigate to company/home if admin/worker
+
+    this.router.navigate(["home"]);
+  }
 }
