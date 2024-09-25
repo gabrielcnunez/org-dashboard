b7807297802813aa7af1d31f820d958b12d5adc9
Keeley Bond <kbond303@gmail.com>
2024-09-25 13:45:49 -0700

    Initial commit for adding cross origin to all controller file endpoints

--- backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
@@ -2,6 +2,7 @@ package com.cooksys.groupfinal.controllers;
 
 import java.util.Set;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,26 +23,31 @@ public class AnnouncementController {
 	private final AnnouncementService announcementService;
 	
 	@GetMapping
+	@CrossOrigin(origins="*")
     public Set<AnnouncementDto> getAllAnnouncements() {
         return announcementService.getAllAnnouncements();
     }
 	
 	@PostMapping("/company/{companyId}")
+	@CrossOrigin(origins="*")
 	public AnnouncementDto createAnnouncement(@RequestBody AnnouncementDto announcementDto, @PathVariable Long companyId) {
 	    return announcementService.createAnnouncement(announcementDto, companyId);
 	}
 	
 	@GetMapping("/company/{companyId}")
+	@CrossOrigin(origins="*")
 	public Set<AnnouncementDto> getAnnouncementsForCompany(@PathVariable Long companyId) {
 	    return announcementService.getAnnouncementsForCompany(companyId);
 	}
 	
 	@GetMapping("/company/{companyId}/user/{userId}/isAdmin")
+	@CrossOrigin(origins="*")
     public boolean isUserAdminForCompany(@PathVariable Long userId, @PathVariable Long companyId) {
         return announcementService.isUserAdminForCompany(userId, companyId);
     }
 
     @GetMapping("/company/{companyId}/user/{userId}/role")
+	@CrossOrigin(origins="*")
     public String getUserRoleForCompany(@PathVariable Long userId, @PathVariable Long companyId) {
         return announcementService.getUserRoleForCompany(userId, companyId);
     }
