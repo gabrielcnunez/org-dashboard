db20385a38fdabdcac40e88bee89f350bca8d6ea
hicksjx2397 <josephdalehicks@outlook.com>
2024-09-24 21:44:19 -0400

    completed project component

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Project from '../models/project';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  projects: Project[] = [{
+    name: 'project1',
+    description: 'this is a project',
+    active: true,
+    team: 1
+    },
+    {
+    name: 'project2',
+    description: 'this is a project also',
+    active: false,
+    team: 2
+    },
+  {
+    name: 'project3',
+    description: 'this is a project',
+    active: true,
+    team: 1
+  }]
+  constructor(private router: Router) {}
 
+  goBack() {
+    
+
+    this.router.navigate(["teams"]);
+  }
 }
