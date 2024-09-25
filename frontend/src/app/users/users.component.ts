4526b4ea98defb37e9581c8e3daa8e038a5d9e00
Philip T <philip1fox1@gmail.com>
2024-09-25 09:17:18 -0700

    users fully works with dummy data

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { UserOverlayComponent } from './user-overlay/user-overlay.component';
 
 @Component({
@@ -6,21 +6,34 @@ import { UserOverlayComponent } from './user-overlay/user-overlay.component';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
-  users = [
-    { name: 'Chris Purnell', email: 'yocrizzle@gmail.com', active: true, admin: true, status: 'JOINED' },
-    { name: 'Kenny Worth', email: 'kmoney@gmail.com', active: true, admin: true, status: 'JOINED' },
-    { name: 'Will Marttala', email: 'wamizzle@gmail.com', active: false, admin: false, status: 'PENDING' },
-    { name: 'Helena Makendengue', email: 'hmasizzle@gmail.com', active: false, admin: false, status: 'PENDING' }
-  ];
+  users = [{ name: '', email: '', active: false, admin: false, status: '' }];
 
   @ViewChild(UserOverlayComponent) overlay!: UserOverlayComponent;
+
+  ngOnInit() {
+    this.loadAllUsers()
+  }
 
   clickAddButton() {
     this.overlay.open();
     if (!this.overlay.checkEmptyFields()) {
       console.log(this.overlay.user)
     }
+  }
+
+  loadAllUsers() {
+    this.users = [
+      { name: 'Chris Purnell', email: 'yocrizzle@gmail.com', active: true, admin: true, status: 'JOINED' },
+      { name: 'Kenny Worth', email: 'kmoney@gmail.com', active: true, admin: true, status: 'JOINED' },
+      { name: 'Will Marttala', email: 'wamizzle@gmail.com', active: false, admin: false, status: 'PENDING' },
+      { name: 'Helena Makendengue', email: 'hmasizzle@gmail.com', active: false, admin: false, status: 'PENDING' }
+    ];
+  }
+
+  addNewUser(newUser: any) {
+    let name = newUser.firstname + ' ' + newUser.lastname;
+    this.users.unshift({ name: name, email: newUser.email, active: true, admin: newUser.admin, status: 'JOINED' });
   }
 }
