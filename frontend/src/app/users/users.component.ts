7d3faf98e16a147d63aef1cea3bf23a2ad7cb02c
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-26 01:15:21 -0500

    Get users from backend

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import { UserOverlayComponent } from './user-overlay/user-overlay.component';
+import {ApiService, BasicUser, Profile, Credentials} from "src/services/api.service";
 
 @Component({
   selector: 'app-users',
@@ -8,12 +9,26 @@ import { UserOverlayComponent } from './user-overlay/user-overlay.component';
 })
 export class UsersComponent implements OnInit {
 
-  users = [{ name: '', email: '', active: false, admin: false, status: '' }];
+  constructor(private apiService: ApiService) { }
+
+  //users = [{ name: '', email: '', active: false, admin: false, status: '' }];
+  users: BasicUser[] = [];
+
+  loggedIn = true;
+  showUsers = false;
+
+  credentials: Credentials  = {
+    "username": "temporaryceoofwaystar",
+    "password": "idontgetpaidenoughforthis"
+  }
 
   @ViewChild(UserOverlayComponent) overlay!: UserOverlayComponent;
 
   ngOnInit() {
-    this.loadAllUsers()
+    //this.loadAllUsers()
+    if (this.loggedIn === true) {
+      this.getAllUsers();
+    }
   }
 
   clickAddButton() {
@@ -23,17 +38,31 @@ export class UsersComponent implements OnInit {
     }
   }
 
-  loadAllUsers() {
-    this.users = [
-      { name: 'Chris Purnell', email: 'yocrizzle@gmail.com', active: true, admin: true, status: 'JOINED' },
-      { name: 'Kenny Worth', email: 'kmoney@gmail.com', active: true, admin: true, status: 'JOINED' },
-      { name: 'Will Marttala', email: 'wamizzle@gmail.com', active: false, admin: false, status: 'PENDING' },
-      { name: 'Helena Makendengue', email: 'hmasizzle@gmail.com', active: false, admin: false, status: 'PENDING' }
-    ];
+  async getAllUsers() {
+    this.apiService.getUsers(this.credentials)
+    .then(data => {
+      this.users = data;
+    })
+    .then(() => this.showUsers = true)
+    .then(() => console.log(this.users))
+    .catch(error => {
+      console.error(error);
+    });
   }
 
+  // loadAllUsers() {
+  //   this.users = [
+  //     { name: 'Chris Purnell', email: 'yocrizzle@gmail.com', active: true, admin: true, status: 'JOINED' },
+  //     { name: 'Kenny Worth', email: 'kmoney@gmail.com', active: true, admin: true, status: 'JOINED' },
+  //     { name: 'Will Marttala', email: 'wamizzle@gmail.com', active: false, admin: false, status: 'PENDING' },
+  //     { name: 'Helena Makendengue', email: 'hmasizzle@gmail.com', active: false, admin: false, status: 'PENDING' }
+  //   ];
+  // }
+
+  // TODO
   addNewUser(newUser: any) {
-    let name = newUser.firstname + ' ' + newUser.lastname;
-    this.users.unshift({ name: name, email: newUser.email, active: true, admin: newUser.admin, status: 'JOINED' });
+    //let name = newUser.firstname + ' ' + newUser.lastname;
+    //this.users.unshift({ name: name, email: newUser.email, active: true, admin: newUser.admin, status: 'JOINED' });
   }
 }
+
--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,10 +1,30 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 
 const usersUrl = 'http://localhost:8080/users/'
 const teamUrl = 'http://localhost:8080/announcements/'
 const companyUrl = 'http://localhost:8080/company/'
 const announcementsUrl = 'http://localhost:8080/announcements/'
+
+export interface Credentials {
+  username: string;
+  password: string;
+}
+
+export interface Profile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+};
+
+export interface BasicUser {
+  id: number;
+  profile: Profile;
+  active: boolean;
+  admin: boolean;
+  status: string;
+};
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +46,19 @@ export class ApiService {
   }
 
   async addNewTeam() {
-    
+
+  }
+
+  async getUsers(credentials: Credentials): Promise<BasicUser[]> {
+    const authCredentials = btoa(`${credentials.username}:${credentials.password}`);
+
+    const headers = new HttpHeaders({
+        'Authorization': `Basic ${authCredentials}`
+    });
+
+    let response = await this.http.get<BasicUser[]>(usersUrl, { headers: headers }).toPromise();
+
+    return response ?? [];
   }
 
 }
