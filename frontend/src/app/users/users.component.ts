692224d40ab0a36a5920b72017185c089b4b9f2f
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-27 09:55:04 -0500

    User display fix

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -87,10 +87,12 @@ export class UsersComponent implements OnInit {
 
   async addNewUser(newUser: any) {
     newUser.companyId = this.companyId;
+    newUser.active = true;
     // These credentials are the currently logged-in admin's credentials
     // The user's credentials are at newUser.user.credentials
     newUser.credentials = this.adminCredentials;
     await this.apiService.createUser(newUser);
+    this.users = [];
     this.getAllUsers();
   }
 }
