8c7390c7b5dae6b1abcc5b8034f750fc660f84b9
Thomas Goodman <26800120+Blazin64@users.noreply.github.com>
2024-09-27 09:12:51 -0500

    Count team projects

--- backend/src/main/java/com/cooksys/groupfinal/mappers/TeamMapper.java
+++ backend/src/main/java/com/cooksys/groupfinal/mappers/TeamMapper.java
@@ -6,10 +6,12 @@ import org.mapstruct.Mapper;
 
 import com.cooksys.groupfinal.dtos.TeamDto;
 import com.cooksys.groupfinal.entities.Team;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = { BasicUserMapper.class })
 public interface TeamMapper {
-	
+
+	@Mapping(target = "projectCount", expression = "java(team.getProjects() != null ? team.getProjects().size() : 0)")
 	TeamDto entityToDto(Team team);
 
 	Set<TeamDto> entitiesToDtos(Set<Team> teams);
