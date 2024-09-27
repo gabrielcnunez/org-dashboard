f1bede4df5d950d9bae544378b1bbba0cb4db3dc
Philip T <philip1fox1@gmail.com>
2024-09-26 19:12:05 -0700

    navbar updates to user status

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -8,7 +8,9 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  isHidden = true;
+  isHidden = false;
+  name = ""
+  admin = false;
 
   constructor(private router: Router) { }
 
@@ -16,6 +18,8 @@ export class NavbarComponent implements OnInit {
     //hides navbar if route is in the login/company page
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
+        this.setName()
+        this.setAdmin()
         this.isHidden = (this.router.url === "/" || this.router.url === "/company");
       }
     });
@@ -23,6 +27,26 @@ export class NavbarComponent implements OnInit {
 
   clicked() {
     // console.log("nav button clicked")
+  }
+
+  setName(): void {
+    if (this.getAdmin()) {
+      this.name = "ADMIN"
+    } else {
+      this.name = this.getName()
+    }
+  }
+
+  setAdmin(): void {
+    this.admin = this.getAdmin();
+  }
+
+  getAdmin() {
+    return JSON.parse(localStorage.getItem("admin") ?? "false");
+  }
+
+  getName() {
+    return JSON.parse(<string>localStorage.getItem("firstName"));
   }
 
 }
